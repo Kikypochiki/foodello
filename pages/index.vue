bf65a1e16bc38dddc9aea86a6a8a407bdf259ad4
Jessie Louise Pongos <jessielouisepongos@gmail.com>
2025-03-11 15:58:07 +0800

    [pages] Add initial Vue application routes

---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+const title = ref("Foodello | Home");
+const description = ref("A Food Inventory System enabling everyone to be more food thrifty");
+
+useSeoMeta({
+ title: () => title.value,
+ description: () => `${title.value} - ${description.value}`,
+ charset: "utf-8",
+ viewport: "width=device-width, initial-scale=1.0",
+ robots: "index, follow",
+
+ ogTitle: () => title.value,
+ ogDescription: () => `${title.value} - ${description.value}`,
+
+ // ogImage: "https://foodello.vercel.app/featured.jpg",
+ // twitterCard: "summary_large_image",
+});
+</script>
+
+<template>
+    <Html lang="en">
+    <Head>
+    <Link rel="icon" type="image/png" href="/logo.png"></Link>
+    <link href="/reset.css" rel="stylesheet" />
+    </Head>
+    </Html>
+   
+    <client-only>
+    <Welcome />
+    </client-only>
+   </template>
+   
+   <style scoped></style>
