9d0ab67132555ea1480c723c6e57e0af231a76d3
Daniel Nino S. Catoy <daniuscatoy@gmail.com>
2025-04-08 15:19:28 +0800

    Add font url and project description

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,32 @@
-<script setup lang="ts">
-const title = ref("Foodello | Home");
-const description = ref("A Food Inventory System enabling everyone to be more food thrifty");
-
-useSeoMeta({
- title: () => title.value,
- description: () => `${title.value} - ${description.value}`,
- charset: "utf-8",
- viewport: "width=device-width, initial-scale=1.0",
- robots: "index, follow",
-
- ogTitle: () => title.value,
- ogDescription: () => `${title.value} - ${description.value}`,
-
- // ogImage: "https://foodello.vercel.app/featured.jpg",
- // twitterCard: "summary_large_image",
-});
-</script>
-
-<template>
-    <Html lang="en">
-    <Head>
-    <Link rel="icon" type="image/png" href="/logo.png"></Link>
-    <link href="/reset.css" rel="stylesheet" />
-    </Head>
-    </Html>
-   
+    <script lang="ts"></script>
+    <template>
     <client-only>
-    <Welcome />
+    <!-- <NavBar /> -->
+    <div id="fdmain" class="fd-main"></div>
+    <!-- <Cookies /> -->
+    <!-- <Copyright /> -->
     </client-only>
-   </template>
-   
-   <style scoped></style>
+    </template>
+    <script setup lang="ts">
+    import { useSeoMeta, useHead } from '@vueuse/head';
+    const title = "Foodello | Home";
+    const description = "A Food Inventory System enabling everyone to be more food thrifty";
+    useSeoMeta({
+    title: () => title,
+    description: () => description,
+    charset: "utf-8",
+    viewport: "width=device-width, initial-scale=1.0"
+    });
+    useHead({
+    link: [
+    {rel: 'icon', type: 'image/png', href: '/logo.png'},
+    {rel: 'stylesheet', href: '/reset.css'},
+    {rel: 'stylesheet', href: '/custom.css'} ,
+    {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+    {rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: ''},
+    {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Underdog&display=swap" rel="stylesheet'}
+    ]
+    });
+    </script>
+    <style scoped></style>
+    <style></style>
